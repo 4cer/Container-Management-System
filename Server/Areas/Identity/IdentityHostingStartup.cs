5be8fb35da0d5ebf1526749de73034582df9fe91
Mateusz Kędzierski <mateusz.kedzierski01@student.wat.edu.pl>
2021-11-25 22:58:06 +0100

    Recovered files lost due to merge

---
+++ Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProITM.Server.Data;
+using ProITM.Server.Models;
+
+[assembly: HostingStartup(typeof(ProITM.Server.Areas.Identity.IdentityHostingStartup))]
+namespace ProITM.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
