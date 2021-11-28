163d7a98dbed8774260200461a7adad8e8533264
Dzamber <dzamber32@gmail.com>
2021-11-28 12:11:00 +0100

    It's not much, but it's a honest work.

--- Server/Controllers/ImageController.cs
+++ Server/Controllers/ImageController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProITM.Server.Data;
 
 namespace ProITM.Server.Controllers
 {
@@ -14,32 +15,61 @@ namespace ProITM.Server.Controllers
         // TODO 156 Inject database ApplicationDbContext
 
         // TODO 147 implement ImageController endpoint methods
+        private readonly ApplicationDbContext db;
+
+        public ImageController(ApplicationDbContext _db)
+        {
+            db = _db;
+            db.Database.EnsureCreated();
+        }
+
 
         [HttpGet("images")]
         public async Task<IActionResult> GetImageList()
         {
-            throw new NotImplementedException("ImageController.GetImageList()");
+            return db.Images.ToList();
+            //throw new NotImplementedException("ImageController.GetImageList()");
         }
 
         [HttpGet("images/{id}")]
         public async Task<IActionResult> GetImageDetails(string imageId)
         {
-            throw new NotImplementedException("ImageController.GetImageDetails()");
+            return db.Images.Find(imageId);
+            //throw new NotImplementedException("ImageController.GetImageDetails()");
         }
+
+
+        [HttpPost("images/upload/{url}")]
+        public async Task<IActionResult> UploadImageFromUrl(string url)
+        {
+            ImageModel model = new ImageModel();
+            model.Id = null;//Tu trzeba to przegadać, bo tu cos niejasne. Imo. tu powinnismy sami generowac jakies Id
+            model.ImageId = null;//A to Id nie powinno byc w bazie, bo na kazdej maszynce bedzie inne
+            //I dopierow przy klikaniu "uzyj obrazu X" powinno byc sciagane na danego dockera, bo tak bedzie prosciej.
+            model.Created = DateTime.Now;
+            model.Name = url;
+            model.Description = "Do uzupelnienia";//To nie wiem, ja jestem naprawde chujowy z weba :C, ktos powiedz mi jak dwa argumenty z url zdjac
+            model.Tag = "latest"; //do sprawdzenia, czy to sie jakos inaczej da.
+            
+            db.Images.Add(model);
+            db.SaveChangesAsync();
+            //throw new NotImplementedException("ImageController.UploadImageFromUdl");
+        }
+
+
+
+
+
+
+
+
+        // Extra-curricular functionality below
 
         [HttpGet("images/users/{id}")]
         public async Task<IActionResult> GetUserImageList(string userId)
         {
             throw new NotImplementedException("ImageController.GetUserImageList()");
         }
-
-        [HttpPost("images/uploadurl")]
-        public async Task<IActionResult> UploadImageFromUrl()
-        {
-            throw new NotImplementedException("ImageController.UploadImageFromUdl");
-        }
-
-        // Extra-curricular functionality below
 
         [HttpPost("images/upload/compiled")]
         public async Task<IActionResult> UploadCompiledImage()
