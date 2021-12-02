313da52f8e84c331770c1f04debfe08f3d9282a1
Varins <marinsans001@gmail.com>
2021-12-02 23:00:51 +0100

    podstawowe zmiany na ContainerService

--- Client/Services/ContainerService.cs
+++ Client/Services/ContainerService.cs
@@ -1,6 +1,7 @@
 ﻿using ProITM.Shared;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -17,24 +18,42 @@ namespace ProITM.Client.Services
             _httpClient = httpClient;
         }
 
-        public async Task<List<ContainerModel>> GetUserContainers(string userId)
+        //TODO POPRAWIĆ JAK BĘDZIE WŁAŚCIWE ZAPYTANIE W KOTNROLERZE
+        public async Task<List<ContainerModel>> ListContainers(long limit)
         {
-            return await _httpClient.GetFromJsonAsync<List<ContainerModel>>($"Container/{userId}");
+            return await _httpClient.GetFromJsonAsync<List<ContainerModel>>($"Container//{limit}");
         }
 
-        public async Task<HttpResponseMessage> StartUsersContainer(string containerId)
+        public async Task<HttpResponseMessage> StartContainer(string containerId)
         {
             return await _httpClient.PostAsJsonAsync<string>($"Container/start/{containerId}", null);
         }
 
-        public async Task<HttpResponseMessage> StopUsersContainer(string containerId)
+        public async Task<HttpResponseMessage> StopContainer(string containerId)
         {
             return await _httpClient.PostAsJsonAsync<string>($"Container/stop/{containerId}", null);
         }
 
-        public async Task<HttpResponseMessage> DeleteUsersContainer(string containerId)
+        public async Task<HttpResponseMessage> DeleteContainer(string containerId)
         {
             return await _httpClient.DeleteAsync($"Container/{containerId}");
+        }
+
+        public async Task<Stream> GetContainerStats(string containerId)
+        {
+            return await _httpClient.GetFromJsonAsync<Stream>($"Container/containers/stats/{containerId}");
+        }
+
+        //To na pewno zadziała? żadnej nazwy ani nic
+        public async Task<HttpResponseMessage> CreateContainer()
+        {
+            return await _httpClient.PostAsJsonAsync<string>($"Container/containers/create", null);
+        }
+
+        //jak przesłać resztę argumentów?
+        public async Task<Stream> GetContainerLogs(string containerId, string since, string tail)
+        {
+            return await _httpClient.GetFromJsonAsync<Stream>($"Container/containers/logs/{containerId}");
         }
     }
 }
--- Client/Services/IContainerService.cs
+++ Client/Services/IContainerService.cs
@@ -9,10 +9,13 @@ namespace ProITM.Client.Services
 {
     interface IContainerService
     {
-        public Task<List<ContainerModel>> GetUserContainers(string userid);
+        public Task<List<ContainerModel>> ListContainers(long limit);
         // TODO may be better to implement as just Task
-        public Task<HttpResponseMessage> StartUsersContainer(string containerid);
-        public Task<HttpResponseMessage> StopUsersContainer(string containerid);
-        public Task<HttpResponseMessage> DeleteUsersContainer(string containerid);
+        public Task<HttpResponseMessage> StartContainer(string containerid);
+        public Task<HttpResponseMessage> StopContainer(string containerid);
+        public Task<System.IO.Stream> GetContainerStats(string containerid);
+        public Task<HttpResponseMessage> CreateContainer();
+        public Task<HttpResponseMessage> DeleteContainer(string containerid);
+        public Task<System.IO.Stream> GetContainerLogs(string containerid, string since, string tail);
     }
 }
