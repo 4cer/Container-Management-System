ab4351d3e1983d188d89451af42a8e9f0e0dd496
Krzysztof Wójcik <krzysztof.wojcik@student.wat.edu.pl>
2021-12-06 20:36:25 +0000

    Merged PR 11: Get container-specific DockerClient instance instead of injected one
    Get container-specific DockerClient instance instead of injected one

---
+++ Server/Utilities/HostModelUtils.cs
@@ -0,0 +1,28 @@
+﻿using Docker.DotNet;
+using ProITM.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProITM.Server.Utilities
+{
+    public static class HostModelUtils
+    {
+        public static DockerClient GetDockerClient(this HostModel host)
+        {
+            string address;
+            if (host.URI != null && host.URI.Length > 0)
+            {
+                address = host.URI;
+            }
+            else
+            {
+                address = $"{host.IP}:{host.Port}";
+            }
+
+            return new DockerClientConfiguration(new Uri(address))
+                .CreateClient();
+        }
+    }
+}
